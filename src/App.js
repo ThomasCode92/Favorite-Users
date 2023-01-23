c9423d5e2daa5f199a638b9d44932457c82e4164
ThomasCode92 <thomascode92@hotmail.com>
2023-01-23 12:37:10 +0100

    React Fragments

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 
 import AddUser from './components/users/AddUser';
 import UsersList from './components/users/UsersList';
@@ -15,10 +15,10 @@ function App() {
   };
 
   return (
-    <div>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </Fragment>
   );
 }
 
